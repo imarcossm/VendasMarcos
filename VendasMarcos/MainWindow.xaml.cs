35dbaa26f21e519a1315c5a644add379694ecdeb
imarcossm <viniciusmorais.com@gmail.com>
2024-06-25 11:16:37 -0300

    first commit

---
+++ VendasMarcos/MainWindow.xaml.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows;
+
+namespace VendasMarcos
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
